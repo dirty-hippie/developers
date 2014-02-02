e321ef4938053970035227a6f50f6cb9de682737
lessless <me@lessless.pp.ua>
2014-02-02 19:22:58 +0200

    update integration changes

--- config/initializers/geokit_config.rb
+++ config/initializers/geokit_config.rb
@@ -3,7 +3,7 @@ if defined? Geokit
 Geokit::default_units = :kms
 Geokit::default_formula = :sphere
 
-Geokit::Geocoders::proxy = nil
+#Geokit::Geocoders::proxy = nil
 Geokit::Geocoders::NominatimGeocoder.server = "nominatim.openstreetmap.org/search"
 Geokit::Geocoders::provider_order = [:nominatim]
 
